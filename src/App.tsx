e93ae6707b951088313cb71f398927e059fb0a42
dnagafonov <denisagafonovwork@gmail.com>
2019-11-22 20:10:14 +0300

    initial and first version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import names from './jsons/firstname.json'
+import surnames from './jsons/lastname.json'
+import ages from './jsons/ages.json'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    peoples: [],
+    name: "",
+    surname: "",
+    age: null,
+  };
+
+  getRandom = (list: Array<any>) => {
+    const randId: any = Math.floor(Math.random() * Math.floor(list.length));
+    return list[randId];
+  };
+
+  getRandomName = () => {
+    this.setState({
+      name: this.getRandom(names)
+    })
+  };
+  getRandomSurname = () => {
+    this.setState({
+      surname: this.getRandom(surnames)
+    })
+  };
+  getRandomAge = () => {
+    this.setState({
+      age: this.getRandom(ages)
+    })
+  };
+
+  createPeople = () => {
+    this.getRandomName();
+    this.getRandomSurname();
+    this.getRandomAge();
+  }
+
+  handleClicked = () => {
+    this.createPeople();
+  };
+  render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
+    return (
+        <div>
+          <div className="header">Radnomize</div>
+          <h2>Name: {this.state.name}</h2>
+          <h2>Surname: {this.state.surname}</h2>
+          <h2>Age: {this.state.age}</h2>
+          <button onClick={this.handleClicked}>click</button>
+        </div>
+    );
+  }
 }
 
 export default App;
